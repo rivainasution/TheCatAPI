23d5e93fc75b96d99a77f7124bacb798a2d85bef
rivainasution <rivai_nasution@student.ub.ac.id>
2022-11-12 18:22:42 +0700

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 
 function Loader() {
     return (
-        <footer>
+        <footer className="mt-3">
             <h4 className="text-center py-2 bg-dark text-light">Allright Reserved. Copyright by Rivai Fansuri Nasution </h4>
         </footer>
     )
