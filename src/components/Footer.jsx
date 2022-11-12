ccb7de1aa392489f96b55b8fa1e4253fd9f8da01
rivainasution <rivai_nasution@student.ub.ac.id>
2022-11-12 18:04:20 +0700

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+function Loader() {
+    return (
+        <footer>
+            <h4 className="text-center py-2 bg-dark text-light">Allright Reserved. Copyright by Rivai Fansuri Nasution </h4>
+        </footer>
+    )
+}
+
+export default Loader;
